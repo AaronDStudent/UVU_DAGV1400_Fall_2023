e95eb5917b1cfc8551c2b225b5c7125c99e789db
Aaron D PC <10876772@uvu.edu>
2023-11-16 06:24:05 -0700

    Created Matching System game and corresponding assignment files.

---
+++ Unity/Matching Systems and More/Assets/Unit7a/Lab/Scripts/ID.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ID : ScriptableObject
+{
+   /*It's one job is to create scriptable objects that exist only for their ID (found in debug mode.
+   So it's purpose has been fulfilled. Nothing else is needed in here since all the ID scriptable object is being used for is it's back end ID*/
+}
